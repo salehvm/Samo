2eb098d77e90898082369daf2f55c85de2e9547c
saleh <salehvm@code.edu.az>
2019-01-18 00:30:38 +0300

    Start Create Samo Pod

---
+++ Samo/Samo/Samo.h
@@ -0,0 +1,19 @@
+//
+//  Samo.h
+//  Samo
+//
+//  Created by Saleh Majıdov on 1/18/19.
+//  Copyright © 2019 Saleh Majıdov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Samo.
+FOUNDATION_EXPORT double SamoVersionNumber;
+
+//! Project version string for Samo.
+FOUNDATION_EXPORT const unsigned char SamoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Samo/PublicHeader.h>
+
+
---
+++ Samo/Samo/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  Samo
+//
+//  Created by Saleh Majıdov on 1/18/19.
+//  Copyright © 2019 Saleh Majıdov. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static func getSomething() -> String {
+        return "New Pod by Saleh Majidov"
+    }
+}
